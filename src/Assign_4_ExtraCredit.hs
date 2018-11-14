bc3feb1143a40079c6b7c46670783d24a625f71e
omar <oalkersh@ymail.com>
2018-11-14 13:25:21 -0500

    finihed most functions, still need comments and to finish binTime

--- src/Assign_4_ExtraCredit.hs
+++ src/Assign_4_ExtraCredit.hs
@@ -1,19 +1,38 @@
 {- Assignment 4 Extra Credit
- - Name: TODO add full name
+ - Name: Omar Alkersh
  - Date: TODO add of completion
  -}
 module Assign_4_ExtraCredit where
 
 import Criterion.Main
 
-macid = "TODO: put your mac id here"
+macid = "alkersho"
 
 
 data Nat = Z
          | S Nat
+         deriving Show
 
 data Digit = Zero
            | One deriving Show
 
 data BinNat = Atom Digit
             | Compound BinNat Digit
+            deriving Show
+
+plus :: Nat -> Nat -> Nat
+plus Z y = y
+plus (S a) y = S (plus a y)
+
+time :: Nat -> Nat -> Nat
+time a (S b) = plus (time a b) a
+time _ _ = Z
+
+binPlus :: BinNat -> BinNat -> BinNat
+binPlus (Atom One) (Atom One) = Compound (Atom One) Zero
+binPlus (Atom a)   (Atom Zero) = Atom a
+binPlus (Compound a One) (Compound x One) = Compound (Compound (binPlus a x) One) One
+binPlus (Compound a b) (Compound x Zero) = Compound (binPlus a x) b
+
+binTime :: BinNat -> BinNat -> BinNat
+binTime a b = error "needs to be implemented"
